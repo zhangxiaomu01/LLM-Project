4d228fba330794c1a366b044ed4c8de3a4caf301
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2024-03-03 20:41:25 +0800

    Adds the first prompt setup.

---
+++ LearningPractice.py
@@ -0,0 +1,45 @@
+### Prompt tuning practice
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+client = openai.OpenAI(
+    # 输入转发API Key
+    api_key=os.getenv('OPENAI_API_KEY'),
+    base_url="https://api.chatkore.com/v1/"
+)
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
+
+
+text = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+prompt = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{text}```
+"""
+response = get_completion(prompt)
+print(response)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LLM-Project
 This is a project repo which is mainly for learning purpose.
+
+1. Install python;
+2. Install dot-env: 'pip install python-dotenv';
+3. Install openai lib with 'py -m pip install openai --upgrade';
+
+Note we are relying on https://www.chatkore.com/index_gpt.html to forward request to openAI.
